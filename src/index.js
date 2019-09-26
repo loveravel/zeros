a23eb2d817de56b9b8ad23f8ad1a7d67758df312
Odintsov Sergei (loveravel) <loveravel@yandex.ru>
2019-09-26 09:36:30 +0400

    init: completed the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
+function getFactorial(n, type) {
+  let result = 1n;
+
+  while (n > 0) {
+    result *= BigInt(n);
+     n -= type;
+  }
+
+  return result;
+}
+
 module.exports = function zeros(expression) {
-  // your solution
+  let result = 1n;
+  let count = 0;
+
+  const values = expression.split('*');
+
+  values.forEach((value) => {
+    value.slice(value.length - 2) === '!!'
+      ? result *= getFactorial(parseInt(value), 2)
+      : result *= getFactorial(parseInt(value), 1);
+  });
+
+  result = result + '';
+  
+  while (result.length) {
+    if (result[result.length - (count + 1)] === '0') {
+      ++count;
+    } else {
+      break;
+    }
+  }
+
+  return count;
 }
